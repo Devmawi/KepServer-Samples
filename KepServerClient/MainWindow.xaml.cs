c3decab13f9a4623f84e3fde554968dcaa1681c0
dev <dev@dev.org>
2023-04-02 15:51:10 +0200

    add opc ua write sample

--- KepServerClient/MainWindow.xaml.cs
+++ KepServerClient/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.Json;
 using System.Text.Unicode;
+using Opc.Ua.Client;
+using Opc.Ua;
 
 namespace KepServerClient
 {
@@ -32,48 +34,50 @@ namespace KepServerClient
         // http://127.0.0.1:39320/iotgateway/read
         private readonly HttpClient _httpClient = new HttpClient() { BaseAddress = new Uri("http://127.0.0.1:39320/iotgateway/") };
         private readonly IMqttClient _mqttClient = new MqttFactory().CreateMqttClient();
+        private Session session;
 
         public MainWindow()
         {
             InitializeComponent();
             SubscribeByMqtt();
+            SubcripeToMonitoredItem();
         }
 
         public async void SubscribeByMqtt()
         {
-       
-                var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
 
-                // Setup message handling before connecting so that queued messages
-                // are also handled properly. When there is no event handler attached all
-                // received messages get lost.
-                _mqttClient.ApplicationMessageReceivedAsync += e =>
+            var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+
+            // Setup message handling before connecting so that queued messages
+            // are also handled properly. When there is no event handler attached all
+            // received messages get lost.
+            _mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                var res = JsonSerializer.Deserialize<IotGatewayMqttMessage>(payload);
+
+                Dispatcher.Invoke(() =>
                 {
-                    var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-                    var res = JsonSerializer.Deserialize<IotGatewayMqttMessage>(payload);
+                    if (cbEnableMqttSubscription.IsChecked == true)
+                        Value.Text = res?.Values.Last().Value.ToString();
+                });
 
-                    Dispatcher.Invoke(() =>
+                return Task.CompletedTask;
+            };
+
+            await _mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+            var mqttSubscribeOptions = new MqttFactory().CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(
+                    f =>
                     {
-                        if(cbEnableMqttSubscription.IsChecked == true)
-                            Value.Text = res?.Values.Last().Value.ToString();
-                    });
-                   
-                    return Task.CompletedTask;
-                };
+                        f.WithTopic("iotgateway");
+                    })
+                .Build();
 
-                await _mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+            await _mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
 
-                var mqttSubscribeOptions = new MqttFactory().CreateSubscribeOptionsBuilder()
-                    .WithTopicFilter(
-                        f =>
-                        {
-                            f.WithTopic("iotgateway");
-                        })
-                    .Build();
 
-                await _mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
-               
-            
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
@@ -93,9 +97,12 @@ namespace KepServerClient
                 }
             };
             var res = await _httpClient.PostAsJsonAsync<IotGatewayWriteRequest[]>("write", iotGatewayWriteRequest);
-            if (!res.IsSuccessStatusCode) {
+            if (!res.IsSuccessStatusCode)
+            {
                 MessageBox.Show("Error :(", "Result", MessageBoxButton.OK, MessageBoxImage.Error);
-            } else {
+            }
+            else
+            {
                 MessageBox.Show("Success :)", "Result", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
@@ -134,6 +141,113 @@ namespace KepServerClient
 
                 await mqttClient.DisconnectAsync();
             }
+        }
+
+        private ApplicationConfiguration BuildOpcUAConfiguration()
+        {
+
+            var config = new ApplicationConfiguration()
+            {
+                ApplicationName = "Custom .NET Client",
+                ApplicationUri = Utils.Format(@"urn:{0}:" + "dotnet-client", "localhost"),
+                ApplicationType = ApplicationType.Client,
+                SecurityConfiguration = new SecurityConfiguration
+                {
+                    //ApplicationCertificate = new CertificateIdentifier { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\MachineDefault", SubjectName = Utils.Format(@"CN={0}, DC={1}", MyApplicationName, ServerAddress) },
+                    //TrustedIssuerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Certificate Authorities" },
+                    //TrustedPeerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Applications" },
+                    RejectedCertificateStore = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\RejectedCertificates" },
+                    AutoAcceptUntrustedCertificates = true,
+                    AddAppCertToTrustedStore = true,
+
+                },
+
+                TransportConfigurations = new TransportConfigurationCollection(),
+                TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+                ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 },
+                TraceConfiguration = new TraceConfiguration()
+            };
+            // config.Validate(ApplicationType.Client).GetAwaiter().GetResult();
+            if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+            {
+                config.CertificateValidator.CertificateValidation += (s, e) => { e.Accept = (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted); };
+            }
+            return config;
+        }
+
+        public async void SubcripeToMonitoredItem() {
+
+            var config = BuildOpcUAConfiguration();
+            string serverAddress = "opc.tcp://127.0.0.1:49320";
+            var selectedEndpoint = CoreClientUtils.SelectEndpoint(serverAddress, useSecurity: false, 15000);
+
+            session = await Session.Create(config, new ConfiguredEndpoint(null, selectedEndpoint, EndpointConfiguration.Create(config)), false, "", 60000, null, null);
+            //session.Open("DOTNET", new UserIdentity() );
+
+            var subscription = new Subscription(session.DefaultSubscription) { PublishingInterval = 1000 };
+
+            //Console.WriteLine("Step 5 - Add a list of items you wish to monitor to the subscription.");
+            var list = new List<MonitoredItem> { };
+            list.Add(new MonitoredItem(subscription.DefaultItem) { DisplayName = "SimChannel.SimDevice.Counter", StartNodeId = "ns=2;s=SimChannel.SimDevice.Counter", });
+
+            list.ForEach(i => i.Notification += OnValueChanged);
+            subscription.AddItems(list);
+
+            //Console.WriteLine("Step 6 - Add the subscription to the session.");
+            session.AddSubscription(subscription);
+            subscription.Create();
+
+           
+        }
+
+
+        // https://github.com/OPCFoundation/UA-.NETStandard/blob/e34eb9b512723e474320300596e92cdb2579f0dc/Applications/ClientControls.Net4/Common/Client/WriteRequestListViewCtrl.cs
+        // https://github.com/OPCFoundation/UA-.NETStandard/blob/4325cd72237bf57ec96e3b97a411a8d18f520612/Applications/ConsoleReferenceClient/ClientSamples.cs
+        private void btnOpcUaWrite_Click(object sender, RoutedEventArgs e)
+        {
+
+            //RequestHeader requestHeader = new RequestHeader()
+            //{
+                
+            //};
+            WriteValueCollection nodesToWrite = new WriteValueCollection();
+            WriteValue intWriteVal = new WriteValue();
+            intWriteVal.NodeId = new NodeId("ns=2;s=SimChannel.SimDevice.Counter");
+            intWriteVal.AttributeId = Attributes.Value;
+            intWriteVal.Value = new DataValue();
+            intWriteVal.Value.Value = int.Parse(Value.Text);
+            nodesToWrite.Add(intWriteVal);
+            //nodesToWrite.Add(new WriteValue()
+            //{
+            //    NodeId = "SimChannel.SimDevice.Counter",
+            //    Value = new DataValue(0)
+            //});
+
+            // read the values.
+            StatusCodeCollection results = null;
+            DiagnosticInfoCollection diagnosticInfos = null;
+            session.Write(null, nodesToWrite, out results, out diagnosticInfos);
+            //await session.CloseAsync();
+        }
+
+        private void OnValueChanged(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            var value = (monitoredItem.LastValue as Opc.Ua.MonitoredItemNotification)?.Value;
+            Dispatcher.Invoke(() =>
+            {
+                if (cbEnableOpcUAMonitoredItem.IsChecked == true)
+                {
+                    
+                    Value.Text = value?.GetValue<int>(0).ToString();
+                }
+                   
+
+            });           
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //session.Close();
         }
     }
 }
